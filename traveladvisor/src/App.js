efbd1993d5175d95d1f8ae3bd9c7ed3d23d74a91
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-30 09:42:14 -0400

    Added imports into my map component and then added styles

--- traveladvisor/src/App.js
+++ traveladvisor/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ backgroundColor: 'red', width: '100%' }}>
+      <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
           <List />
         </Grid>
--- traveladvisor/src/components/map/Map.js
+++ traveladvisor/src/components/map/Map.js
@@ -1,3 +1,9 @@
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import { Rating } from '@material-ui/lab'
+
+import useStyles from './styles'
 
 const Map = () => {
   return (
