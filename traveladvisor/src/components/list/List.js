8324293332d3eb21180f465d49a3dee4379ffeb2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-30 10:26:40 -0400

    Added imports into my list component and then added styles

--- traveladvisor/src/components/list/List.js
+++ traveladvisor/src/components/list/List.js
@@ -1,3 +1,8 @@
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core"
+
+import useStyles from './styles'
+
+
 
 const List = () => {
   return (
--- traveladvisor/src/components/map/Map.js
+++ traveladvisor/src/components/map/Map.js
@@ -1,13 +1,30 @@
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
-import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined'
 import { Rating } from '@material-ui/lab'
 
 import useStyles from './styles'
 
 const Map = () => {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width: 600px)')
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      {/* <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyA5CTtpwtKmj4K5AzTEa4E-ZFOr6qesw2U' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact> */}
+    </div>
   )
 }
 
