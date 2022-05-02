49c25ad37e0e44b51591627d1b2a212a59043375
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-02 11:27:29 -0400

    Added the ratings option to filter by

--- traveladvisor/src/components/list/List.js
+++ traveladvisor/src/components/list/List.js
@@ -10,9 +10,15 @@ const List = () => {
   const classes = useStyles()
 
   const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+
 
   const handleChange = (event) => {
     setType(event.target.value)
+  }
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value)
   }
 
   return (
@@ -26,6 +32,15 @@ const List = () => {
           <MenuItem value='restaurants'>Restaurants</MenuItem>
           <MenuItem value='hotels'>Hotels</MenuItem>
           <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={handleRatingChange}>
+          <MenuItem value={0}>All Ratings</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
     </div>
